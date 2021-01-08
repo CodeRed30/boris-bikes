ef6f27ff20df169f19e6d234d90a8091b32bc439
PetarSimonovic <petar@simonovic.com>
2021-01-08 15:37:53 +0000

    Refactored testing

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,16 +4,18 @@ require 'bike'
 describe DockingStation do
     it { is_expected.to respond_to(:release_bike) }
 
-    # it 'releases a new instance of a bike in response to release_bike' do
-    #   expect(subject.release_bike).to be_an_instance_of Bike
-    # end
+    it 'releases a new instance of a bike in response to release_bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to be_an_instance_of Bike
+    end
 
     it { is_expected.to respond_to(:dock).with(1).argument }
 
-    # it 'returns the instance of the bike in response to dock' do
-    #   bike = Bike.new
-    #   expect(subject.dock(bike)).to eq bike
-    # end
+    it 'returns the instance of the bike in response to dock' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+    end
 
     it { is_expected.to respond_to(:bike) }
 
