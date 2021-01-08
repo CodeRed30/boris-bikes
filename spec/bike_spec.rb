2eacc3ca4e1854a76cf3eab7fde5269ce5d4d3bf
PetarSimonovic <petar@simonovic.com>
2021-01-08 17:39:35 +0000

    Function added to report broken bikes

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,9 +1,12 @@
 require 'bike'
 
 describe Bike do
-    it { is_expected.to respond_to(:working?) }
+    it { is_expected.to respond_to(:broken?) }
 
-    it 'returns true if bike is working' do
-      expect(subject.working?).to eq true
+    it { is_expected.to respond_to(:report_broken) }
+
+    it 'returns true if bike is broken' do
+      subject.report_broken
+      expect(subject).to be_broken
     end
 end
